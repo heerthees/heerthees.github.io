4e32a1c557f1d0e12b127e131b098fe9b1e9322f
unknown <Administrator@LT108763.fareast.corp.microsoft.com>
2019-04-08 22:33:58 +0530

    Updates

---
+++ precache-manifest.e38617d37c73ca955fe2e569e1c0a8ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "863c6b833dd1207609ee",
+    "url": "/static/css/main.8b75d2a4.chunk.css"
+  },
+  {
+    "revision": "863c6b833dd1207609ee",
+    "url": "/static/js/main.863c6b83.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "00215f61d73b84163fc1719a5c871f30",
+    "url": "/index.html"
+  }
+];
