8479b29bf67b91d05bbdde3f796168db9950159a
unknown <Administrator@LT108763.fareast.corp.microsoft.com>
2019-04-08 22:25:20 +0530

    INtial commit

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# heerthees.github.io
-I am a Software Developer who is passonate to learn new tehnologies. 
-So, here i develop few application to test my understanding on the technologies i learn.
+This is my Portfolio React Application.
---
+++ src/resumeData.js
@@ -0,0 +1,66 @@
+let resumeData = {
+    "imagebaseurl":"https://heerthees.github.io/",
+    "name": "Heerthees",
+    "role": "Full Stack Python Developer",
+    "linkedinId":"Heertheeswaran",
+    "skypeid": "heerthees@gmail.com",
+    "roleDescription": "I like wandering in various parts of full stack development and like to learn about new technologies, read technical articles or play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://in.linkedin.com/in/heertheeswaran-v-7457b0104",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/heerthees",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/heertheeswaran",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"A passionate developer who is instrested in solving real time problems and develop products which helps the mankind.",
+    "address":"India",
+    "website":"https://heerthees.github.io",
+    "work":[
+      {
+        "CompanyName":"Cognizant",
+        "specialization":"Microsoft Finance Busineess Intellegience",
+        "joining":"May 2018",
+        "leaving":"Till Date",
+        "Achievements":"Best ELT of 2018"
+      },
+    ],
+    "skillsDescription":"My skills are here",
+    "skills":[
+      {
+        "skillname":"Python"
+      },
+      {
+        "skillname":"Django"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"RubyonRails"
+      },
+    ],
+    "portfolio":[
+    ],
+    "testimonials":[
+      {
+        "description":"Everybody should learn to program a computer, because it teaches you how to think.",
+        "name":"Steve Jobs"
+      },
+      {
+        "description":"I taught myself how to program computers when I was a kid, bought my first computer when I was 10, and sold my first commercial program when I was 12",
+        "name":"Elon Musk"
+      }
+    ]
+  }
+  
+  export default resumeData
